64e21ffdd4f8a2aaf06668ed31584b2e835b723d
Yeswan S <yeswan.333.s@gmail.com>
2026-07-18 10:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.67%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,9 +4,10 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        res = [[0] * n for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
-                res[j][n - i - 1] =  matrix[i][j]
+        for i in range(0 , n - 1):
+            for j in range(i + 1 , n):
+                matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
         
-        matrix[:] = res
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j] , matrix[i][n - j - 1] = matrix[i][n - j - 1] , matrix[i][j]
