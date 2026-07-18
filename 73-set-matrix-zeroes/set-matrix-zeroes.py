df3a1463351000c2bb924e54be193901f8a62acf
Yeswan S <yeswan.333.s@gmail.com>
2026-07-18 10:04:05 +0530

    Time: 7 ms (58.83%) | Memory: 20.8 MB (12.28%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,16 +4,25 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         r , c = len(matrix) , len(matrix[0])
-        rows = [0] * r
-        cols = [0] * c
+        col0 = 1
         for i in range(r):
             for j in range(c):
                 if matrix[i][j] == 0:
-                    rows[i] = 1
-                    cols[j] = 1
+                    matrix[i][0] = 0
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
         
-        for i in range(r):
-            for j in range(c):
-                if rows[i] or cols[j]:
+        for i in range(1 , r):
+            for j in range(1 , c):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
         
+        if matrix[0][0] == 0:
+            for j in range(c):
+                matrix[0][j] = 0
+        
+        if col0 == 0:
+            for i in range(r):
+                matrix[i][0] = 0
