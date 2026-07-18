285f981df417f12393f12deaebfcfa1e0e3f6e50
Yeswan S <yeswan.333.s@gmail.com>
2026-07-18 10:10:54 +0530

    Time: 8 ms (35.17%) | Memory: 21 MB (10.76%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,7 +4,9 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         r , c = len(matrix) , len(matrix[0])
+
         col0 = 1
+
         for i in range(r):
             for j in range(c):
                 if matrix[i][j] == 0:
@@ -14,11 +16,11 @@ class Solution:
                     else:
                         col0 = 0
         
-        for i in range(1 , r):
+        for i in range(1, r):
             for j in range(1 , c):
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-        
+
         if matrix[0][0] == 0:
             for j in range(c):
                 matrix[0][j] = 0
