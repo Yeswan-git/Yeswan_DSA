ea0d5afaeb055cb078246458d6c9d1a084bfeead
Yeswan S <yeswan.333.s@gmail.com>
2026-07-20 22:59:37 +0530

    Time: 12 ms (17.13%) | Memory: 22.3 MB (68.17%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,32 +1,33 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
         el1 = el2 = None
         cnt1 = cnt2 = 0
-        length = len(nums)
-        for n in nums:
-            if cnt1 == 0 and n != el2:
-                el1 = n
+
+        for num in nums:
+            if cnt1 == 0 and num != el2:
+                el1 = num
                 cnt1 = 1
-            elif cnt2 == 0 and n != el1:
-                el2 = n
+            elif cnt2 == 0 and num != el1:
+                el2 = num
                 cnt2 = 1
-            elif n == el1:
+            
+            elif num == el1:
                 cnt1 += 1
-            elif n == el2:
+            elif num == el2:
                 cnt2 += 1
+            
             else:
                 cnt1 -= 1
                 cnt2 -= 1
         
-        count1 = count2 = 0
-        for n in nums:
-            if n == el1:
-                count1 += 1
-            elif n == el2:
-                count2 += 1
         res = []
-        if count1 > length // 3:
-            res.append(el1)
-        if count2 > length // 3:
-            res.append(el2)
+        cnt1 = cnt2 = 0
+        for num in nums:
+            if num == el1: cnt1 += 1
+            elif num == el2: cnt2 += 1
+        
+        if cnt1 > n // 3 : res.append(el1)
+        if cnt2 > n // 3 : res.append(el2)
+
         return res
