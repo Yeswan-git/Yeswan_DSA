da54ce5c8fe378d5420d374b00d35cc394534fe3
Yeswan S <yeswan.333.s@gmail.com>
2026-07-20 22:52:08 +0530

    Time: 11 ms (34.26%) | Memory: 22.4 MB (62.53%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,9 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        freq = Counter(nums)
+        el1 = el2 = None
+        cnt1 = cnt2 = 0
+        length = len(nums)
+        for n in nums:
+            if cnt1 == 0 and n != el2:
+                el1 = n
+                cnt1 = 1
+            elif cnt2 == 0 and n != el1:
+                el2 = n
+                cnt2 = 1
+            elif n == el1:
+                cnt1 += 1
+            elif n == el2:
+                cnt2 += 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        
+        count1 = count2 = 0
+        for n in nums:
+            if n == el1:
+                count1 += 1
+            elif n == el2:
+                count2 += 1
         res = []
-        for k , v in freq.items():
-            if v > n // 3 :
-                res.append(k)
+        if count1 > length // 3:
+            res.append(el1)
+        if count2 > length // 3:
+            res.append(el2)
         return res
