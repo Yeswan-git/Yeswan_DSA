f91b8ce075a5359ee6813cef8c136516952c596f
Yeswan S <yeswan.333.s@gmail.com>
2026-07-28 20:38:29 +0530

    Time: 8 ms (76.12%) | Memory: 21.1 MB (27.80%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -2,17 +2,6 @@ class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
         actual_sum = n * (n + 1) // 2
-
-        array_sum = 0
-        for e in nums:
-            array_sum += e
-        
-        unique_sum = 0
-        s = set(nums)
-        for e in s:
-            unique_sum += e
-        
-        miss = actual_sum - unique_sum
-        dup = array_sum - unique_sum
-
-        return [dup , miss]
+        arr_sum = sum(n for n in nums)
+        unique_sum = sum(n for n in set(nums))
+        return [arr_sum - unique_sum , actual_sum - unique_sum]
