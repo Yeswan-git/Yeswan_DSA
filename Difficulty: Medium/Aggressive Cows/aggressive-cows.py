b0126e7898f32162b5b16771826d30ba8f8d71e8
Yeswan S <yeswan.333.s@gmail.com>
2026-08-19 09:20:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.py
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.py
@@ -2,19 +2,20 @@ class Solution:
     def aggressiveCows(self, arr, k):
         arr.sort()
         n = len(arr)
-        def can_we_place(min_dist):
-            cnt_cows = 1
+        def can_we_place(min_val):
+            cow_cnt = 1
             last = arr[0]
             for i in range(1 , n):
-                if arr[i] - last >= min_dist:
-                    cnt_cows += 1
+                if arr[i] - last >= min_val :
+                    cow_cnt += 1
                     last = arr[i]
-            return cnt_cows >= k
-        l , r = 1 , max(arr) - min(arr)
+            return cow_cnt >= k
+            
         res = 1
+        l , r = 1 , 10 ** 8
         while l <= r :
             mid = l + (r - l) // 2
-            if can_we_place(mid):
+            if can_we_place(mid) :
                 res = mid
                 l = mid + 1
             else:
