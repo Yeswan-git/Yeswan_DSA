51e756563ef4c961aadb92bffc9354aed5a0c47f
Yeswan S <yeswan.333.s@gmail.com>
2026-08-25 07:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.35%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -2,9 +2,9 @@ class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
         smallest = k
         m = 1
-
+        new_nums = set(nums)
         while True :
-            if smallest in nums :
+            if smallest in new_nums :
                 m += 1
                 smallest = m * k
             
