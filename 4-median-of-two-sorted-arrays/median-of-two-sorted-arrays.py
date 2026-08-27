275281bd79b9ebaa5ac5a57f1177282970684fd2
Yeswan S <yeswan.333.s@gmail.com>
2026-08-27 19:40:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (77.98%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,29 +1,38 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         n1 , n2 = len(nums1) , len(nums2)
-        nums = []
+        n = n1 + n2
+
+        idx1 = n // 2 - 1
+        idx2 = n // 2
+        cnt = 0
+        el1 = el2 = -1
         i = j = 0
-        while i < n1 and j < n2:
+        while i < n1 and j < n2 :
             if nums1[i] <= nums2[j]:
-                nums.append(nums1[i])
+                if cnt == idx1 : el1 = nums1[i]
+                if cnt == idx2 : el2 = nums1[i]
+                cnt += 1
                 i += 1
             else:
-                nums.append(nums2[j])
+                if cnt == idx1 : el1 = nums2[j]
+                if cnt == idx2 : el2 = nums2[j]
+                cnt += 1
                 j += 1
-        while i < n1:
-            nums.append(nums1[i])
+            if el1 != -1 and el2 != -1 : break
+        
+        while i < n1 :
+            if cnt == idx1 : el1 = nums1[i]
+            if cnt == idx2 : el2 = nums1[i]
+            cnt += 1
             i += 1
-        while j < n2:
-            nums.append(nums2[j])
+            if el1 != -1 and el2 != -1 : break
+        while j < n2 :
+            if cnt == idx1 : el1 = nums2[j]
+            if cnt == idx2 : el2 = nums2[j]
+            cnt += 1
             j += 1
+            if el1 != -1 and el2 != -1 : break
         
-        n = n1 + n2
-        if n % 2 == 0:
-            median = (nums[n // 2] + nums[n // 2 - 1]) / 2
-        else:
-            median = nums[n // 2]
-
-        return median
-
-
-        
+        if n & 1 : return el2
+        else : return (el1 + el2) / 2
