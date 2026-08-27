9a269c323bf1df32e06e9abeecbf324ccfbc51b7
Yeswan S <yeswan.333.s@gmail.com>
2026-08-27 21:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (95.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,38 +1,25 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         n1 , n2 = len(nums1) , len(nums2)
+        if n1 > n2 : return self.findMedianSortedArrays(nums2 , nums1)
+        l , r = 0 , n1
+        left = (n1 + n2 + 1) // 2
         n = n1 + n2
+        while l <= r :
+            mid1 = (l + r) >> 1
+            mid2 = left - mid1
+            l1 = l2 = float("-inf")
+            r1 = r2 = float("inf")
+            if mid1 - 1 >= 0 : l1 = nums1[mid1 - 1]
+            if mid2 - 1 >= 0 : l2 = nums2[mid2 - 1]
+            if mid1 < n1 : r1 = nums1[mid1]
+            if mid2 < n2 : r2 = nums2[mid2]
 
-        idx1 = n // 2 - 1
-        idx2 = n // 2
-        cnt = 0
-        el1 = el2 = -1
-        i = j = 0
-        while i < n1 and j < n2 :
-            if nums1[i] <= nums2[j]:
-                if cnt == idx1 : el1 = nums1[i]
-                if cnt == idx2 : el2 = nums1[i]
-                cnt += 1
-                i += 1
-            else:
-                if cnt == idx1 : el1 = nums2[j]
-                if cnt == idx2 : el2 = nums2[j]
-                cnt += 1
-                j += 1
-            if el1 != -1 and el2 != -1 : break
+            if l1 <= r2 and l2 <= r1 : 
+                if n & 1 : return max(l1 , l2)
+                else : return (max(l1 , l2) + min(r1 , r2)) / 2
+            
+            elif l1 > r2 : r = mid1 - 1
+            else : l = mid1 + 1
         
-        while i < n1 :
-            if cnt == idx1 : el1 = nums1[i]
-            if cnt == idx2 : el2 = nums1[i]
-            cnt += 1
-            i += 1
-            if el1 != -1 and el2 != -1 : break
-        while j < n2 :
-            if cnt == idx1 : el1 = nums2[j]
-            if cnt == idx2 : el2 = nums2[j]
-            cnt += 1
-            j += 1
-            if el1 != -1 and el2 != -1 : break
-        
-        if n & 1 : return el2
-        else : return (el1 + el2) / 2
+        return -1
