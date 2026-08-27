0bafce550ed4548fd07844bc1139b6c9f82ea108
Yeswan S <yeswan.333.s@gmail.com>
2026-08-27 22:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (95.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -2,9 +2,11 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         n1 , n2 = len(nums1) , len(nums2)
         if n1 > n2 : return self.findMedianSortedArrays(nums2 , nums1)
-        l , r = 0 , n1
-        left = (n1 + n2 + 1) // 2
         n = n1 + n2
+        left = (n1 + n2 + 1) >> 1
+        l , r = 0 , n1
+
+
         while l <= r :
             mid1 = (l + r) >> 1
             mid2 = left - mid1
@@ -15,11 +17,10 @@ class Solution:
             if mid1 < n1 : r1 = nums1[mid1]
             if mid2 < n2 : r2 = nums2[mid2]
 
-            if l1 <= r2 and l2 <= r1 : 
+            if l1 <= r2 and l2 <= r1 :
                 if n & 1 : return max(l1 , l2)
                 else : return (max(l1 , l2) + min(r1 , r2)) / 2
             
             elif l1 > r2 : r = mid1 - 1
             else : l = mid1 + 1
-        
         return -1
