bf3a46c6328cc80b387d651b35899cf358fae379
Yeswan S <yeswan.333.s@gmail.com>
2026-08-30 15:53:12 +0530

    Time: 7 ms (91.62%) | Memory: 19.4 MB (45.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # return sorted(s) == sorted(t)
-        from collections import Counter
-        return Counter(s) == Counter(t)
+        # from collections import Counter
+        # return Counter(s) == Counter(t)
+        if len(s) != len(t) : return False
+
+        count = [0] * 26
+
+        for c1 , c2 in zip(s , t):
+            count[ord(c1) - ord("a")] += 1
+            count[ord(c2) - ord("a")] -= 1
+        
+        return all(x == 0 for x in count)
