9f420f76547943e3793400684cffd83a39bb6c2a
Yeswan S <yeswan.333.s@gmail.com>
2026-09-03 12:20:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.py
@@ -0,0 +1,15 @@
+'''Structure of Linked List Node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def searchKey(self, head, key):
+        curr = head
+        while curr :
+            if curr.data == key :
+                return True
+            curr = curr.next
+        return False
