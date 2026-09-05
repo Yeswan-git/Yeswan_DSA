0208906993d0556672d8f3aca885a0931e11afd2
Yeswan S <yeswan.333.s@gmail.com>
2026-09-05 08:41:00 +0530

    Time: 275 ms (14.09%) | Memory: 31.9 MB (79.55%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
@@ -2,9 +2,10 @@ class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         n = len(nums)
         maxs = [0] * n
-        maxs[0] = nums[0]
         mins = [0] * n
+        maxs[0] = nums[0]
         mins[n - 1] = nums[n - 1]
+
         for i in range(1 , n):
             maxs[i] = max(maxs[i - 1] , nums[i])
             mins[n - i - 1] = min(mins[n - i] , nums[n - i - 1])
@@ -12,5 +13,5 @@ class Solution:
         for i in range(n):
             if maxs[i] - mins[i] <= k :
                 return i
-        
+            
         return -1
