5be881f3cff7ccf0f693226e39a51c8808c1bbf4
Yeswan S <yeswan.333.s@gmail.com>
2026-09-11 08:26:28 +0530

    Time: 640 ms (5.05%) | Memory: 19.2 MB (72.53%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,19 +1,16 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        uniques = set()
-        n = len(digits)
+        count = 0
+        freq = Counter(digits)
 
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    if i != j and j != k and i != k :
-                        hun = digits[i]
-                        ten = digits[j]
-                        uni = digits[k]
+        for i in range(100 , 1000 , 2):
+            hun = i // 100
+            ten = (i // 10) % 10
+            uni = i % 10
 
-                        if hun != 0 and uni % 2 == 0 :
-                            num = hun * 100 + ten * 10 + uni
+            needed = Counter([hun , ten , uni])
 
-                            uniques.add(num)
+            if all(freq[d] >= needed[d] for d in needed):
+                count += 1
         
-        return len(uniques)
+        return count
