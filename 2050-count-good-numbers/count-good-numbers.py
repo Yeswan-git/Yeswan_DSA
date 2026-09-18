cadc0f70157c48f3366d792ca784b5412cdca7d1
Yeswan S <yeswan.333.s@gmail.com>
2026-09-18 21:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (24.18%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,16 +1,17 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10 ** 9 + 7
-        def Pow(x , n):
-            res = 1
-            x %= MOD
 
-            while n :
-                if n & 1 :
-                    res = (res * x) % MOD
+        def Pow(base , exp):
+            res = 1
+            base %= MOD
+
+            while exp :
+                if exp & 1 :
+                    res = (res * base) % MOD
                 
-                x = (x * x) % MOD
-                n = n // 2
+                base = (base * base) % MOD
+                exp = exp // 2
             
             return res % MOD
         
@@ -18,4 +19,3 @@ class Solution:
         odd_idxs = n - even_idxs
 
         return (Pow(5 , even_idxs) * Pow(4 , odd_idxs)) % MOD
-        
