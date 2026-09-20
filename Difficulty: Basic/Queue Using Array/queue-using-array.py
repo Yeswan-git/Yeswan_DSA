663a492a04012aa111582f127e4567cc99f4e0a6
Yeswan S <yeswan.333.s@gmail.com>
2026-09-20 10:43:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Queue Using Array/queue-using-array.py
@@ -0,0 +1,46 @@
+class myQueue:
+    def __init__(self, n):
+        self.arr = [-1] * n
+        self.size = n
+        self.front = 0
+        self.rear = -1
+        self.count = 0
+
+    
+    def isEmpty(self):
+        return self.count == 0
+
+    
+    def isFull(self):
+        return self.count == self.size
+
+    
+    def enqueue(self, x):
+        if self.isFull():
+            return 
+        self.rear = (self.rear + 1) % self.size
+        self.arr[self.rear] = x
+        self.count += 1
+
+    
+    def dequeue(self):
+        if self.isEmpty():
+            return
+        self.arr[self.front] = -1
+        self.front = (self.front + 1) % self.size
+        self.count -= 1
+        
+
+    
+    def getFront(self):
+        if self.isEmpty():
+            return -1
+        return self.arr[self.front]
+       
+    
+    def getRear(self):
+        if self.isEmpty():
+            return -1
+        return self.arr[self.rear]
+        
+        
